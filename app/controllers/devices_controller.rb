a6c0731621d30c0eb47fd9055fc0da1e2e77c1a3
Gokeii <21250546@qq.com>
2013-06-11 19:38:56 +0800

    change device controller to change device owner when device already exists

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,15 @@
 class DevicesController < ApplicationController
 	before_filter :authenticate_user!
 	def create
-		device = current_user.build_device
-		device.registration_id = params[:registration_id]
-		device.save
+		device = Device.where(:registration_id => params[:registration_id]).first
+		if device.nil?
+			device = current_user.build_device
+			device.registration_id = params[:registration_id]
+			device.save
+		else
+			device.user = current_user
+			device.save
+		end
 
 		render  :status => 200,
 						:json => 	{ :success => true,
